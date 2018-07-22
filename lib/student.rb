ceea1a279c34f30542dd61615e0d4f52bb37aa3b
Adam Barton <adam.barton333@gmail.com>
2018-07-22 00:58:57 +0000

    automatically backed up by learn

---
+++ lib/student.rb
@@ -0,0 +1,16 @@
+class Student 
+
+  attr_accessor :name 
+
+  def initialize(name)
+    @name = name 
+  end
+
+  def hello 
+    "Hey there! I'm so excited to learn stuff."
+  end
+
+  def raise_hand
+    "Pick me!"
+
+end
